184ca882006aa7077df3dc4887db3ea361acc9a6
藤澤篤志 <fujisawaatsushi@fujisawaatsushinoMacBook-Pro.local>
2017-12-13 23:15:57 +0900

    work front module

--- AqoursApp/_dev-front/src/App.vue
+++ AqoursApp/_dev-front/src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div id="app">
+    <header-view />
     <img src="./assets/logo.png">
     <router-view/>
+    <div class="button">test</div>
   </div>
 </template>
 
 <script>
+import HeaderView from './components/globals/Header/Header'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    HeaderView
+  }
 }
 </script>
-
+<style src="./assets/font-awesome/css/font-awesome.min.css"></style>
+<style lang="scss" src="./components/globals/style/style.scss"></style>
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
---
+++ AqoursApp/_dev-front/src/components/globals/Header/Header.vue
@@ -0,0 +1,7 @@
+<template src="./header.html"></template>
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+<style src="./header.scss" lang="scss"></style>
