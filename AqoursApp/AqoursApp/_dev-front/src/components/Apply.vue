55cb34d1c5bca1265317c6e69582880fd34b5a78
f-a24 <>
2018-08-19 22:55:54 +0900

    Add Schedule Board Apply File

---
+++ AqoursApp/AqoursApp/_dev-front/src/components/Apply.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Apply</h1>
+</template>
--- AqoursApp/AqoursApp/_dev-front/src/router/index.js
+++ AqoursApp/AqoursApp/_dev-front/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '../components/HelloWorld';
+import Schedule from '../components/Schedule';
+import Apply from '../components/Apply';
+import Board from '../components/Board';
+import File from '../components/File';
 import Member from '../components/Member/Member';
 
 Vue.use(Router);
@@ -13,10 +17,31 @@ export default new Router({
       name: 'HelloWorld',
       component: HelloWorld
     },
+,
+    {
+      path: '/Schedule',
+      name: 'Schedule',
+      component: Schedule
+    },
     {
       path: '/Member',
       name: 'Member',
       component: Member
+    },
+    {
+      path: '/Apply',
+      name: 'Apply',
+      component: Apply
+    },
+    {
+      path: '/Board',
+      name: 'Board',
+      component: Board
+    },
+    {
+      path: '/File',
+      name: 'File',
+      component: File
     }
   ]
 });
