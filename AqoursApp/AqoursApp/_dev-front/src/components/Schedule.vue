ef758b6bb21a08b246f5f6acd024512e6de3d7c0
f-a24 <>
2018-08-19 23:13:48 +0900

    Schedule implementation

--- AqoursApp/AqoursApp/_dev-front/src/components/Schedule.vue
+++ AqoursApp/AqoursApp/_dev-front/src/components/Schedule.vue
@@ -1,3 +1,20 @@
 <template>
-  <h1>Schedule</h1>
+    <v-date-picker
+      v-model="picker"
+      :landscape="true"
+      :reactive="false"
+      full-width="true" />
 </template>
+<script>
+  export default {
+    data: () => ({ picker: null })
+  }
+</script>
+<style lang="scss">
+.v-date-picker-header {
+  .v-icon {
+    font-size: 2rem!important;
+    padding: 0!important;
+  }
+}
+</style>
